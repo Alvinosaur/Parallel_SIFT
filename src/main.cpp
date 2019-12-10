3db44bf53b9857897b5da1d82f7db7e682ae1db5
Alvinosaur <ashek@andrew.cmu.edu>
2019-12-10 01:29:12 -0500

    functional parallel demo

--- src/main.cpp
+++ src/main.cpp
@@ -65,44 +65,44 @@ int main(int argc, char* argv[]){
 
     find_keypoint_features(src1, features1, keypoints1, num_tasks, rank,
         reqs, stats);
-    // find_keypoint_features(src2, features2, keypoints2, num_tasks, rank,
-    //     reqs, stats);
+    find_keypoint_features(src2, features2, keypoints2, num_tasks, rank,
+        reqs, stats);
 
+    if (rank == 0) {
+        //-- Step 3: Matching descriptor vectors using FLANN matcher
+        cv::FlannBasedMatcher matcher;
+        std::vector< cv::DMatch > matches;
+        matcher.match( features1, features2, matches );
+
+        double max_dist = 0; double min_dist = 100;
+
+        // //-- Quick calculation of max and min distances between keypoints
+        for( int i = 0; i < features1.rows; i++ )
+        { double dist = matches[i].distance;
+            if( dist < min_dist ) min_dist = dist;
+            if( dist > max_dist ) max_dist = dist;
+        }
+
+        //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
+        //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
+        //-- small)
+        //-- PS.- radiusMatch can also be used here.
+        std::vector< cv::DMatch > good_matches;
+
+        for( int i = 0; i < features1.rows; i++ )
+        { if( matches[i].distance <= max(2*min_dist, 0.02) )
+            { good_matches.push_back( matches[i]); }
+        }
+
+        //-- Draw only "good" matches
+        drawMatches( src1_mat, keypoints1, src2_mat, keypoints2,
+            good_matches, res_output, cv::Scalar::all(-1), cv::Scalar::all(-1),
+            vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+            
+        imshow( "Blurred pikachu!", res_output );
+        cv::waitKey(0);
+    }
     MPI_Finalize();
-
-    // //-- Step 3: Matching descriptor vectors using FLANN matcher
-    // cv::FlannBasedMatcher matcher;
-    // std::vector< cv::DMatch > matches;
-    // matcher.match( features1, features2, matches );
-
-    // double max_dist = 0; double min_dist = 100;
-
-    // // //-- Quick calculation of max and min distances between keypoints
-    // for( int i = 0; i < features1.rows; i++ )
-    // { double dist = matches[i].distance;
-    //     if( dist < min_dist ) min_dist = dist;
-    //     if( dist > max_dist ) max_dist = dist;
-    // }
-
-    // //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
-    // //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
-    // //-- small)
-    // //-- PS.- radiusMatch can also be used here.
-    // std::vector< cv::DMatch > good_matches;
-
-    // for( int i = 0; i < features1.rows; i++ )
-    // { if( matches[i].distance <= max(2*min_dist, 0.02) )
-    //     { good_matches.push_back( matches[i]); }
-    // }
-
-    // //-- Draw only "good" matches
-    // drawMatches( src1_mat, keypoints1, src2_mat, keypoints2,
-    //     good_matches, res_output, cv::Scalar::all(-1), cv::Scalar::all(-1),
-    //     vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-
-    // imwrite( "after_blur_result.jpg", res_output);
-    // imshow( "Blurred pikachu!", res_output );
-    // cv::waitKey(0);
     return 0;
 }
 
@@ -137,32 +137,23 @@ void find_keypoint_features(Image & src, cv::Mat & result_features,
     find_kp_time += kp_finder.find_keypoints(octave2_log, octave2_kp);
     find_kp_time += kp_finder.find_keypoints(octave3_log, octave3_kp);
     find_kp_time += kp_finder.find_keypoints(octave4_log, octave4_kp);
-    printf("keypoint_find for ALL octaves time: %.3f s\n", find_kp_time);
 
     if (rank == 0) {
-        cv::Mat res_output;
-        octave1_kp[0].store_opencv(res_output);
-        imshow( "Blurred pikachu!", res_output );
-        cv::waitKey(0);
+        Image gradx(src.rows, src.cols), grady(src.rows, src.cols);
+        std::vector<coord> keypoints;
+
+        std::vector<PointWithAngle> points_with_angle;
+        SIFT_TIME = kp_finder.find_corners_gradients(
+            octave1_kp[view_index], keypoints, points_with_angle);
+
+        std::vector<float> kp_gradients;
+
+        std::vector<KeypointFeature> keypoint_features;
+        kp_finder.find_keypoint_orientations(keypoints, points_with_angle, 
+            keypoint_features, src.rows, src.cols, standard_variances[2]);
+
+        kp_finder.store_keypoints(keypoint_features, cv_keypoints, 1, src.cols);
+        
+        kp_finder.store_features(keypoint_features, result_features);
     }
-
-    // Image gradx(src.rows, src.cols), grady(src.rows, src.cols);
-    // std::vector<coord> keypoints;
-
-    // std::vector<PointWithAngle> points_with_angle;
-    // SIFT_TIME = kp_finder.find_corners_gradients(
-    //     octave1_kp[view_index], keypoints, points_with_angle);
-    // printf("corner detection for octave1 time: %.3f s\n", SIFT_TIME);
-
-    // std::vector<float> kp_gradients;
-
-    // std::vector<KeypointFeature> keypoint_features;
-    // kp_finder.find_keypoint_orientations(keypoints, points_with_angle, 
-    //     keypoint_features, src.rows, src.cols, standard_variances[2]);
-
-    // kp_finder.store_keypoints(keypoint_features, cv_keypoints, 1, src.cols);
-    
-    // kp_finder.store_features(keypoint_features, result_features);
-
-
 }
